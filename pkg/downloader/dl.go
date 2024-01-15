4d9fa39135ae75be86197b9914d498a418f7c733
Sergey Yakubovskiy <sergey.yakubovskiy@cenomi.com>
2024-01-15 11:38:24 +0300

    updated download function

--- pkg/downloader/dl.go
+++ pkg/downloader/dl.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 )
 
-const maxRetries = 3
-const delayBetweenRetries = time.Second * 5
+const (
+	maxRetries          = 3
+	delayBetweenRetries = time.Second * 5
+)
 
 func DownloadFile(url string) (io.Reader, error) {
 	var resp *http.Response
@@ -22,4 +27,40 @@ func DownloadFile(url string) (io.Reader, error) {
 		time.Sleep(delayBetweenRetries)
 	}
 	return nil, fmt.Errorf("failed to download file after %d attempts, last error: %s", maxRetries, err)
+}
+
+func DownloadFileToTmp(url string) (string, error) {
+	var resp *http.Response
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		resp, err = http.Get(url)
+		if err == nil {
+			break
+		}
+		time.Sleep(delayBetweenRetries)
+	}
+	if err != nil {
+		return "", fmt.Errorf("failed to download file after %d attempts, last error: %s", maxRetries, err)
+	}
+	defer resp.Body.Close()
+
+	// Extract filename from URL
+	filename := path.Base(url)
+	filePath := filepath.Join("/tmp", filename)
+
+	// Create file
+	file, err := os.Create(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// Write the body to file
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
 }
